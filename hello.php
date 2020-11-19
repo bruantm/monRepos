891d848ed5f2e48e10280fe904388341b22e74c4
bruantm <74680217+bruantm@users.noreply.github.com>
2020-11-20 00:11:00 +0100

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello SIO2</title>
+    </head>
+    <body>
+        <p>
+			Exemple pour le TD sur Git :
+			
+            <?php echo 'Hello la classe !'; ?>
+			
+			Je m'appelle Edouard !
+        </p>
+    </body>
+</html>
